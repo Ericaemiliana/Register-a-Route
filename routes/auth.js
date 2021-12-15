c6f16a1c59544cb26c1fcb831fc22f1bf7ade275
erica pantoja <ericap@marsreturnship.com>
2021-12-15 18:54:16 -0500

    adding main files

---
+++ routes/auth.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const router = new express.Router();
+const ExpressError = require("../expressError");
+const db = require("../db");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { BCRYPT_WORK_FACTOR, SECRET_KEY } = require("../config");
+const { ensureLoggedIn, ensureAdmin } = require("../middleware/ath");
+
+router.get("/", (req, res, next) => {
+  res.send("APP IS WORKING!!!");
+});
+
+router.post("/register", async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    if (!username || !password) {
+      throw new ExpressError("Username and password required", 400);
+    }
+    // hash password
+    const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
+    // save to db
+    const results = await db.query(
+      `
+      INSERT INTO users (username, password)
+      VALUES ($1, $2)
+      RETURNING username`,
+      [username, hashedPassword]
+    );
+    return res.json(results.rows[0]);
+  } catch (e) {
+    if (e.code === "23505") {
+      return next(
+        new ExpressError("Username taken. Please pick another!", 400)
+      );
+    }
+    return next(e);
+  }
+});
+
+module.exports = router;
